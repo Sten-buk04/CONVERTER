7841ea44a45e574023e3b25794a1c8319b6f7824
Stenbuk <stenbuk44.4@gmail.com>
2026-04-19 23:27:44 +0100

    commit JS

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ function convertMarkdown(){
   text = text.replace(/^## (.*$)/gim, "<h2>$1</h2>");
   text = text.replace(/^# (.*$)/gim, "<h1>$1</h1>");
 
-  text = text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
-  text = text.replace(/\*(.*?)\*/g, "<em>$1</em>");
+  text = text.replace(/__(.*?)__/gim, "<strong>$1</strong>");
+  text = text.replace(/\*\*(.*?)\*\*/gim, "<strong>$1</strong>");
+
+  text = text.replace(/\*(.*?)\*/gim, "<em>$1</em>");
+  text = text.replace(/_(.*?)_/gim, "<em>$1</em>");
 
   text = text.replace(/!\[(.*?)\]\((.*?)\)/g, '<img alt="$1" src="$2">');
   text = text.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
 
   text = text.replace(/^\s*> (.*$)/gim, "<blockquote>$1</blockquote>");
-
+  
   return text;
 }
 
